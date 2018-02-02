5ce8c15baf66147ec1cf9e916032588dee367b7c
Armen Zambrano G <armenzg@gmail.com>
2018-02-02 15:32:45 -0500

    Tell the server to send all routes from the root

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -8,6 +8,12 @@ module.exports = {
           title: 'neutrino_experiments'
         }
       }
-    ]
+    ],
+    (neutrino) => {
+      // Read https://stackoverflow.com/a/36623117
+      // This is the key to making React Router work with neutrino
+      // Fix issue with nested routes e.g /index/garbage
+      neutrino.config.output.publicPath('/');
+    }
   ]
 };
